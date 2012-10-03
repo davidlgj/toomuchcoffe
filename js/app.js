457e2ac4e2d4c314b815385fffe8f70d299cdfa6
David Jensen <david.lgj@gmail.com>
2012-10-03 22:50:54 +0200

    Simplified (no half cups)  and added touch events via QUOJs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+
+//Copyright (c) 2012 David Jensen
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy 
+//of this software and associated documentation files (the "Software"), to deal 
+//in the Software without restriction, including without limitation the rights 
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//copies of the Software, and to permit persons to whom the Software is 
+//furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in 
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+//SOFTWARE.
+
+
 "use strict";
 //init service module
 angular.module('app',[])
@@ -65,66 +86,99 @@ angular.module('app').factory('coffe', ['storage',function(storage) {
         storage.set('dates',dates)
     }
     
-    //debug
-    storage.set(today,[])
-    
     /**
      * Cups of coffe today
      * @return {number}
      */
     coffe.today = function(){
-        return _.reduce(storage.get(today,[]), function(memo, cup){ return memo + cup; }, 0)
+        return storage.get(today,0)
     }
     
     /**
-     * Add a cup (or half a cup)
-     * @param {boolean} whole (optional) true == one cup, false == half a cup, default: true
+     * Add a cup 
      */
-    coffe.add = function(whole) {
-        var cups = storage.get(today,[])
-        cups.push(whole === false?0.5:1)
+    coffe.add = function() {
+        var cups = storage.get(today,0)
+        cups++
         storage.set(today,cups)
     }
-    
+        
     /**
-     * Change last added cup
-     * @param {boolean} whole
-     */
-    coffe.change = function(whole){
-        coffe.pop()
-        coffe.add(whole)
-    }
-    
-    /**
-     * Pop last added cup
+     * Decrease one cup
      * @return {Object} the cup
      */
-    coffe.pop = function(whole){
-        var cups = storage.get(today,[])
-        var cup = cups.pop()
-        storage.set(today,cups)
-        return cup
+    coffe.dec = function(){
+        var cups = storage.get(today,0)
+        cups--
+        storage.set(today,Math.max(0,cups))
+        return cups
+    }
+    
+    /**
+     * Reset today to 0
+     */ 
+    coffe.reset = function(){
+        storage.set(today,0)
     }
     
     return coffe
 }])
 
-
+/**
+ * Directives
+ */
+var makeDirective = function(event){
+    angular.module('app').directive(event, function factory($parse) {
+        var def = {
+            restrict: 'A',
+            replace: false,
+            transclude: false,
+            
+            link: function(scope, element, attrs) {
+                $$(element[0]).on(event,function(e){
+                    console.log('event fired',e)
+                    e.preventDefault();
+                    
+                    var fn = $parse(attrs[event])
+                    scope.$apply(function() {
+                        fn(scope)
+                    })
+                })
+            }
+        }
+        return def
+    })
+}
+ 
+makeDirective('tap')
+makeDirective('hold')
+ 
 
 /**
  * Controllers
  */ 
 function CoffeCtrl($scope,coffe) {
-    
+
+    //hacks
+    //to prevent default longclick (hold) behaviour, i.e. the dialog for saving an image
+    //we have to preventDefault
+    document.addEventListener('touchstart',function(e){ e.preventDefault() },false)
+
     //preload all images
     for (var i=0; i<16; i++) {
         var img = new Image()
-        img.src = 'img/coffecups/'+i+'.jpg';
+        img.src = 'img/coffecups/'+i+'.jpg'
     }
     
     $scope.add = function() {
         coffe.add()
         $scope.cupimage = 'cup'+ Math.min(15,$scope.count())
+    }
+    
+    
+    $scope.reset = function() {
+        coffe.reset()
+        $scope.cupimage = 'cup0'
     }
     
     $scope.count = function(){
