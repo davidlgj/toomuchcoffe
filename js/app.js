7009b8fbc1769a7984a0edee7815c685006b762a
David Jensen <david.lgj@gmail.com>
2013-01-06 23:00:07 +0100

    Statistics and both low and high quality images.

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ angular.module('app').factory('storage', [function() {
     storage.get = function(key,def) {
         var val = window.localStorage[key]
         if (angular.isUndefined(val) || val === 'undefined') {
-            if (def) {
+            if (angular.isDefined(def)) {
                 return def;
             } 
             return undefined;
@@ -67,11 +67,18 @@ angular.module('app').factory('storage', [function() {
     }
     
     return storage
-}])
 
-angular.module('app').factory('coffe', ['storage',function(storage) {
+}]).factory('coffe', ['$filter','storage',function($filter,storage) {
     var coffe = {} 
-    
+    var days = {
+        'Mon': 0,
+        'Tue': 1,
+        'Wed': 2,
+        'Thu': 3,
+        'Fri': 4,
+        'Sat': 5,
+        'Sun': 6
+    }
     var today = new Date().toDateString()
     
     //init storage
@@ -121,7 +128,39 @@ angular.module('app').factory('coffe', ['storage',function(storage) {
         storage.set(today,0)
     }
     
+    /**
+     * Stats, one week a time
+     * @param week {number} (optional) week to fetch from storage, defaults to current week
+     * @return {Object} With the weekdays as attributes, object {date,count}
+     */ 
+    coffe.week = function(date){
+        if (!date) {
+            date = new Date()
+        }
+        var monday = date.getTime() - days[$filter('date')(date,'EEE')]*86400000
+        
+        var construct = function(nr) {
+            var d = new Date(monday+86400000*nr)
+            return {
+                date: d,
+                count: storage.get(d.toDateString())
+            }
+        } 
+        
+        return {
+            monday: construct(0),    
+            tuesday: construct(1),
+            wednesday: construct(2),
+            thursday: construct(3),
+            friday: construct(4),
+            saturday: construct(5),
+            sunday: construct(6),
+        }
+        
+    }
+    
     return coffe
+
 }])
 
 /**
@@ -135,18 +174,20 @@ var makeDirective = function(event){
             transclude: false,
             
             link: function(scope, element, attrs) {
-                $$(element[0]).on(event,function(e){
+                var handler = function(e){
                     console.log('event fired',e)
+                    e.stopPropagation();
                     e.preventDefault();
                     
                     var fn = $parse(attrs[event])
                     scope.$apply(function() {
                         fn(scope)
                     })
-                })
+                }
+                $$(element[0]).on(event,handler)
             }
         }
-        return def
+        return def;
     })
 }
  
@@ -159,15 +200,20 @@ makeDirective('hold')
  */ 
 function CoffeCtrl($scope,coffe) {
 
+    $scope.page = 'counter'
+
     //hacks
     //to prevent default longclick (hold) behaviour, i.e. the dialog for saving an image
     //we have to preventDefault
+    //only do it on phonegap otherwise it blocks chromes "emulate touch events"
     document.addEventListener('touchstart',function(e){ e.preventDefault() },false)
-
+    
     //preload all images
     for (var i=0; i<16; i++) {
+        console.log('preload ' + i)
         var img = new Image()
         img.src = 'img/coffecups/'+i+'.jpg'
+        
     }
     
     $scope.add = function() {
@@ -186,4 +232,47 @@ function CoffeCtrl($scope,coffe) {
     }
     
     $scope.cupimage = 'cup'+ Math.min(15,$scope.count())
+    
+    //init variables
+    $scope.stats = false
+    
+    
+    $scope.showCounter = function() {
+        $scope.page = 'counter'
+    }
+    
+    $scope.showStats = function() {
+        $scope.page = 'stats'
+    }
+}
+
+function StatsCtrl($scope,coffe) {
+    Date.prototype.getWeek = function() {
+        var onejan = new Date(this.getFullYear(),0,1)
+        return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7)
+    }
+    
+    
+    $scope.date = new Date()
+    
+    
+    var update = function () {
+        $scope.week = $scope.date.getWeek()
+        angular.forEach(coffe.week($scope.date),function(v,k){
+            console.log(k,v)
+            $scope[k] = angular.isDefined(v.count)?v.count:'-'
+        })
+    };
+    
+    update();
+    
+    $scope.prev = function(){
+        $scope.date = new Date($scope.date.getTime() - 86400000*7)
+        update()
+    }
+    
+    $scope.next = function(){
+        $scope.date = new Date($scope.date.getTime() + 86400000*7)
+        update()
+    }
 }
